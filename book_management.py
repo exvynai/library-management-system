24fc6ed9bdc2b03284530326ee959c56ddb869d5
exvynai <vir200319@gmail.com>
2022-07-14 16:48:50 +0530

    added edit book function

--- book_management.py
+++ book_management.py
@@ -4,7 +4,7 @@ class Book:
         author = input("Enter the name of the Author: ")
         publisher = input("Enter the name of the Publisher: ")
         year = input("Enter the year of publishing: ")
-        price = input("Enter the price of the book")
+        price = input("Enter the price of the book: ")
         school_name = input("Enter the name of the school: ")
 
         return f"{title};{author};{publisher};{year};{price};{school_name}\n"
@@ -25,6 +25,8 @@ class Book:
             file = open('data/books.txt', 'a+')
             file.write(bookinfo)
             file.close()
+
+            print("Book added Successfully!")
         else:
             print("Book already exists!")
 
@@ -48,12 +50,42 @@ class Book:
 
         print("BOOK DELETED SUCCESSFULLY!")
 
+    def edit_book(self, bookname, author):
+        if self.search_book(bookname, author) == "Book doesn't exist!":
+            print("The book doesn't exist!")
+        else:
+            file = open("data/books.txt", "r+")
+            for line in file:
+                data = line.split(";")
+                if data[0] == bookname and data[1] == author:
+                    while True:
+                        choice = input("Which attribute would you like to edit?\n1 - Title\n2 - Publisher\n3 - Year\n4 - Price\n5 - School name\n6 - Quit\nEnter your choice: ")
+                        if choice == '1' or choice.lower() == 'title':
+                            data[0] = input("Enter new book name: ")
+                            print(f"Successfully changed title to {data[0]}")
+                        elif choice == '2' or choice.lower() == 'publisher':
+                            data[1] = input("Enter new publisher: ")
+                            print(f"Successfully changed publisher to {data[1]}")
+                        elif choice == '3' or choice.lower() == 'year':
+                            data[2] = input("Enter new year: ")
+                            print(f"Successfully changed year to {data[2]}")
+                        elif choice == '4' or choice.lower() == 'price':
+                            data[3] = input("Enter new price: ")
+                            print(f"Successfully changed price to {data[3]}")
+                        elif choice == '5' or choice.lower() == 'school name':
+                            data[4] = input("Enter new school name: ")
+                            print(f"Successfully changed school name to {data[4]}")
+                        elif choice == '6' or choice.lower() == 'quit':
+                            break
+                        else:
+                            print("Invalid choice!")
+        
 def main():
     b = Book()
     # b.add_book()
     # print(b.search_book("My book", "viraj"))
     # b.delete_book("My book", "viraj")
-
+    b.edit_book("my book", "viraj")
 
 if __name__ == '__main__':
     main()
